05fa18e9dd2947c64f1ff4020bc78cf7ab33168b
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-02-16 12:01:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function showSuccess(input){
     formControl.className = "form-control success";
 }
 
-//Het Field Name For 
+//Het Field Name Fo
 function getFieldName(input){
     return input.id.charAt(0).toUpperCase()+ input.id.slice(1);
 }
